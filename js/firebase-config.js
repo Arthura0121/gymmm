1ca6e562e2182bc5f61ea472b8cf40a030b1951a
Arthura0121 <100959964+Arthura0121@users.noreply.github.com>
2026-09-04 20:47:32 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,11 +6,12 @@
    ============================================================ */
 
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+    apiKey: "AIzaSyCoA3wAy3gwKwa49Xhw5HFhs7qcATP1648",
+  authDomain: "gymm-3be17.firebaseapp.com",
+  databaseURL: "https://gymm-3be17-default-rtdb.firebaseio.com",
+  projectId: "gymm-3be17",
+  storageBucket: "gymm-3be17.firebasestorage.app",
+  messagingSenderId: "161689386687",
+  appId: "1:161689386687:web:672a2fddf85761190c900a",
+  measurementId: "G-M4K32Q04YH"
 };
